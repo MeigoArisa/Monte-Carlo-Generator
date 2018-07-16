5b7e4945350e9184a9dee12b3f2a0965634d81c0
DESKTOP-TS8U10B\김강현 <wwponv158@gmail.com>
2018-07-16 13:28:35 +0900

    optimizing

--- Monte Carlo/RandomDataGen.cs
+++ Monte Carlo/RandomDataGen.cs
@@ -13,38 +13,18 @@ namespace Monte_Carlo
         int speedvalue =0;
         int Weavingvalue =0;
         int intervalvalue =0;
-
         public static string strFilePath = "";
         string strSeperator = ",";
-        
-        //double 형의 난수를 생성하기 위한 메소드
-        //min값이 max값이 넘지 않는지 체크 후 난수결과값 리턴
-        public double NextDouble(double min, double max, Random seed)
-        {
-            Random random = seed;
-            if (min == max)
-            {
-                return min;
-            }
-            if (min > max)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
-            double returnsum = (random.NextDouble() * (Math.Abs(max - min)) + min);
-            random = null;
-            return returnsum;
-        }
-
 
         public async Task<int> speedfile(int value)
         {
+            RandomPool.rdPoolStaticAllocLen = value;
             speedvalue = value;
             const int NumOfThread = 1;
 
             await Task.Factory.StartNew(() => 
             {
                 Thread[] threads = new Thread[NumOfThread];
-
                 for (int i = 0; i < NumOfThread; i++)
                 {
                     threads[i] = new Thread(Work_speed);
@@ -73,22 +53,28 @@ namespace Monte_Carlo
 
         public void Work_speed(object threadId)
         {
+            RandomPool radpool = new RandomPool();
             Point pos = new Point();
             string FilePath = strFilePath + "testfilespeed.csv";
             string Output = null;
             StringBuilder sbAddress = new StringBuilder();
-            unsafe
+            lock (radpool.rdPoolLock)
             {
-                for (int i = 0; i < speedvalue; i++)
+                unsafe
                 {
-                    pos.X = NextDouble(0.0000001, 65, new Random(-500 + i));
-                    pos.Y = NextDouble((Math.Pow(0.97, pos.X) * (500)) - 50, Math.Pow(0.97, pos.X) * (500) + 50, new Random(1 * i));
-                    fixed (double* fixedX = &pos.X, fixedY = &pos.Y)
+                    for (int i = 0; i < speedvalue; i++)
                     {
-                        sbAddress.Append(string.Join(strSeperator, *fixedX, *fixedY, Environment.NewLine));
+                        pos.X = radpool.NextDouble(0.0000001, 65);
+                        pos.Y = radpool.NextDouble((Math.Pow(0.97, pos.X) * (500)) - 50, Math.Pow(0.97, pos.X) * (500) + 50);
+                        fixed (double* fixedX = &pos.X, fixedY = &pos.Y)
+                        {
+                            sbAddress.Append(string.Join(strSeperator, *fixedX, *fixedY, Environment.NewLine));
+                        }
                     }
                 }
             }
+            radpool.rdPoolSwappable = true;
+            radpool.rdPoolOffset = RandomPool.rdPoolStaticAllocLen;
             Output = sbAddress.ToString();
             File.AppendAllText(FilePath, Output);
             sbAddress = null;
@@ -98,6 +84,7 @@ namespace Monte_Carlo
         public async Task<int> intervaltestfile(int value)
         {
             intervalvalue = value;
+            RandomPool.rdPoolStaticAllocLen = value;
             const int NumOfThread = 1;
 
             await Task.Factory.StartNew(() =>
@@ -127,24 +114,28 @@ namespace Monte_Carlo
         
         public unsafe void Work_interval(object threadId)
         {
+            RandomPool radpool = new RandomPool();
             string FilePath = strFilePath + "intervaltestfile.csv";
             Point pos = new Point();
             string Output = null;
             StringBuilder sbAddress = new StringBuilder();
-
-            unsafe
+            lock (radpool.rdPoolLock)
             {
-                for (var i = 0; i < intervalvalue; i++)
+                unsafe
                 {
-                    pos.X = NextDouble(0.0000001, 60, new Random(-30+i));
-                    pos.Y = NextDouble((-pos.X) / 12 + 30, (-pos.X) / 12 + 40, new Random(1 * i));
-                    fixed (double* fixedX = &pos.X, fixedY = &pos.Y)
+                    for (var i = 0; i < intervalvalue; i++)
                     {
-                        sbAddress.Append(string.Join(strSeperator, *fixedX, *fixedY, Environment.NewLine));
+                        pos.X = radpool.NextDouble(0.0000001, 60);
+                        pos.Y = radpool.NextDouble((-pos.X) / 12 + 30, (-pos.X) / 12 + 40);
+                        fixed (double* fixedX = &pos.X, fixedY = &pos.Y)
+                        {
+                            sbAddress.Append(string.Join(strSeperator, *fixedX, *fixedY, Environment.NewLine));
+                        }
                     }
                 }
             }
-
+            radpool.rdPoolSwappable = true;
+            radpool.rdPoolOffset = RandomPool.rdPoolStaticAllocLen;
             Output = sbAddress.ToString();
             File.AppendAllText(FilePath, Output);
             sbAddress = null;
@@ -155,6 +146,7 @@ namespace Monte_Carlo
         public async Task<int> Weavingtestfile(int value)
         {
             Weavingvalue = value;
+            RandomPool.rdPoolStaticAllocLen = value;
             const int NumOfThread = 1;
 
             await Task.Factory.StartNew(() =>
@@ -185,23 +177,28 @@ namespace Monte_Carlo
         
         public unsafe void Work_Weaving(object threadId)
         {
+            RandomPool radpool = new RandomPool();
             string FilePath = strFilePath + "Weavingtestfile.csv";
             Point pos = new Point();
             string Output = null;
             StringBuilder sbAddress = new StringBuilder();
-
-            unsafe
+            lock (radpool.rdPoolLock)
             {
-                for (var i = 0; i < Weavingvalue; i++)
+                unsafe
                 {
-                    pos.X = NextDouble(0.0000001, 60, new Random(-7+i));
-                    pos.Y = NextDouble((Math.Pow(pos.X, 2) * 0.25) - 7, ((Math.Pow(pos.X, 2) * 0.25) + 7), new Random(1 * i));
-                    fixed (double* fixedX = &pos.X, fixedY = &pos.Y)
+                    for (var i = 0; i < Weavingvalue; i++)
                     {
-                        sbAddress.Append(string.Join(strSeperator, *fixedX, *fixedY, Environment.NewLine));
+                        pos.X = radpool.NextDouble(0.0000001, 60);
+                        pos.Y = radpool.NextDouble((Math.Pow(pos.X, 2) * 0.25) - 7, ((Math.Pow(pos.X, 2) * 0.25) + 7));
+                        fixed (double* fixedX = &pos.X, fixedY = &pos.Y)
+                        {
+                            sbAddress.Append(string.Join(strSeperator, *fixedX, *fixedY, Environment.NewLine));
+                        }
                     }
                 }
             }
+            radpool.rdPoolSwappable = true;
+            radpool.rdPoolOffset = RandomPool.rdPoolStaticAllocLen;
             Output = sbAddress.ToString();
             File.AppendAllText(FilePath, Output);
             sbAddress = null;
---
+++ Monte Carlo/RandomPool.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+
+namespace Monte_Carlo
+{
+    public class RandomPool
+    {
+        public static int rdPoolStaticAllocLen = 1000000;
+
+        private Random   rdDevice = new Random();
+
+        public int      rdPoolOffset;
+        private Double[] rdPoolCur;
+
+        public Object   rdPoolLock = new object();
+        public bool     rdPoolSwappable;
+        private Double[] rdPoolNew;
+
+        unsafe public RandomPool()
+        {
+            rdDevice = new Random();
+
+            lock (rdPoolLock)
+            {
+                rdPoolCur = new double[rdPoolStaticAllocLen];
+                rdPoolNew = new double[rdPoolStaticAllocLen];
+
+                unsafe
+                {
+                    fixed (Double* curPool = rdPoolCur, newPool = rdPoolNew)
+                    {
+                        for (int i = 0; i < rdPoolStaticAllocLen; ++i)
+                        {
+                            curPool[i] = rdDevice.NextDouble();
+                            newPool[i] = rdDevice.NextDouble();
+                        }
+                    }
+                }
+
+                rdPoolSwappable = true;
+                rdPoolOffset = rdPoolStaticAllocLen;
+            }
+        }
+
+        ~RandomPool()
+        {
+            lock (rdPoolLock)
+            {
+                rdPoolCur = null;
+                rdPoolNew = null;
+            }
+        }
+
+        private void Swap(ref Double[] lhs, ref Double[] rhs)
+        {
+            Double[] tmp = lhs;
+            lhs = rhs;
+            rhs = lhs;
+        }
+
+        private void GenerateNewPool()
+        {
+            unsafe
+            {
+                fixed (Double* newPool = rdPoolNew)
+                {
+                    for (int i = 0; i < rdPoolStaticAllocLen; ++i)
+                    {
+                        newPool[i] = rdDevice.NextDouble();
+                    }
+                }
+            }
+
+            rdPoolSwappable = true;
+        }
+
+        private Double NextDoubleImpl()
+        {
+            lock (rdPoolLock)
+            {
+                if (rdPoolOffset == 0)
+                {
+                    while (!rdPoolSwappable)
+                    {
+                        Thread.Yield();
+                    }
+
+                    Swap(ref rdPoolCur, ref rdPoolNew);
+                    rdPoolSwappable = false;
+                    rdPoolOffset = rdPoolStaticAllocLen;
+
+                    new Thread(GenerateNewPool);
+                }
+
+                return rdPoolCur[--rdPoolOffset];
+            }
+        }
+
+        public Double NextDouble(Double minRange, Double maxRange)
+        {
+            return NextDoubleImpl() * (Math.Abs(maxRange - minRange) + minRange);
+        }
+    }
+}
